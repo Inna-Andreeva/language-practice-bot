5a888ba3fd3903cf1b6afe0b9c07f77436631e77
Anna <efluviri@gmail.com>
2026-09-25 16:09:59 +0500

    изменила

--- src/handlers/impl/CamelCaseHandler.java
+++ src/handlers/impl/CamelCaseHandler.java
@@ -1,19 +1,19 @@
 package handlers.impl;
 import handlers.TextHandler;
-public class CamelCaseHandler implements TextHandler{
+public class CamelCaseHandler implements TextHandler {
+    @Override
     public String handle(String input){
         if (input == null || input.isEmpty()) return input;
         String[] words = input.split(" ");
-        String result = words[0].toLowerCase();
+        StringBuilder result = new StringBuilder(words[0].toLowerCase());
         for (int i = 1; i < words.length; i++) {
             String word = words[i];
             if (!word.isEmpty()) {
                 String firstLetter = word.substring(0, 1).toUpperCase();
                 String restOfTheWord = word.substring(1).toLowerCase();
-                String fixedWord = firstLetter + restOfTheWord;
-                result=result+fixedWord;
+                result.append(firstLetter).append(restOfTheWord);
             }
         }
-        return result;
+        return result.toString();
     }
 }
--- src/handlers/impl/UpperCaseHandler.java
+++ src/handlers/impl/UpperCaseHandler.java
@@ -1,6 +1,7 @@
 package handlers.impl;
 import handlers.TextHandler;
 public class UpperCaseHandler implements TextHandler {
+    @Override
     public String handle(String input){
         return input.toUpperCase();
     }
